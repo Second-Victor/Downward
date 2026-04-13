63653c6ef26ba9015f46dc12936e3015629a0596
Allan Davis <allan.victor.davis@icloud.com>
2026-04-13 08:23:54 +0100

    Initial Commit

---
+++ Downward/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Downward
+//
+//  Created by Allan Davis on 13/04/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Downward/DownwardApp.swift
@@ -0,0 +1,17 @@
+//
+//  DownwardApp.swift
+//  Downward
+//
+//  Created by Allan Davis on 13/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct DownwardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
